0c8312e8668cb3202dd66b2aec9b6eba7219261e
Dominick27 <43536180+Dominick27@users.noreply.github.com>
2018-09-26 13:53:54 +0200

    Initial commit

---
+++ Assets/_GameAssets/Scrips/PipeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeGenerator : MonoBehaviour {
+
+    [SerializeField] Transform prefabTuberia;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("GeneratePipe", 0, 0.75f);
+	}
+	
+	// Update is called once per frame
+
+	void GeneratePipe () {
+        //Debug.Log("tuberia");
+
+        // GENERAMOS TUBERIAS
+        if (GameConfig.IsPlaying())
+        {
+            Instantiate(prefabTuberia, transform.position, Quaternion.identity);
+        }
+	}
+  }
---
+++ Assets/_GameAssets/Scrips/Zombie.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Zombie : MonoBehaviour {
+    [SerializeField] ParticleSystem prefabblood;
+    [SerializeField] float fuerza = 8f;
+    [SerializeField] Text score;
+
+    private Rigidbody rb;
+    private int puntuacion = 0;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameConfig.ArrancaJuego();
+        rb = GetComponent<Rigidbody>();
+        ActualizarScore();
+    }
+
+    
+    // Update is called once per frame
+
+
+    void Update () {
+		if (Input.GetKeyDown("space"))
+         {
+             rb.AddForce(Vector3.up * fuerza);
+
+            /*rb.velocity = Vector3.zero;
+            rb.AddForce(new Vector3(0, fuerza, 0)); */
+
+            //Debug.Log("Ha pulsado space");
+        }
+	}
+
+    private void OnTriggerEnter(Collider other){
+        //Debug.Log("ha pasado");
+        puntuacion++;
+        ActualizarScore();
+        //Debug.Log("Score: " + puntuacion);
+        
+    }
+    private void OnCollisionEnter(Collision collision) {
+
+        //DETEN EL JUEGO
+
+        GameConfig.ParaJuego();
+
+        //CREAR SISTEMA DE PARTICULAS
+        Instantiate(prefabblood, transform.position, Quaternion.identity);
+
+        // DESACTIVAR EL RENDER
+        gameObject.SetActive(false);
+
+        //LLAMAR A FINALIZAR PARTIDA
+        Invoke("FinalizarPartida", 2.5f);
+
+    }
+    private void FinalizarPartida() {
+        Destroy(this.gameObject);
+        SceneManager.LoadScene(0);
+    }
+    private void ActualizarScore()
+    {
+        score.text = "Score: " + puntuacion.ToString();
+    }
+}
+
